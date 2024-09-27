bf74925dfeaee9d233e31df37e8930792d74f7b0
Gevorg Petrosyan <armen.kharatyuan@menugroup.co.uk>
2024-09-27 09:51:32 +0400

    implementing routes in a new war with react ruter 6.4+

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+/* eslint-disable no-unused-vars */
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -35,46 +35,46 @@ function CreateOrder() {
   const cart = fakeCart;
 
   return (
-    <div>
-      <h2>Ready to order? Let's go!</h2>
+		<div>
+			<h2>Ready to order? Let&aposs go!</h2>
 
-      <form>
-        <div>
-          <label>First Name</label>
-          <input type="text" name="customer" required />
-        </div>
+			<form>
+				<div>
+					<label>First Name</label>
+					<input type="text" name="customer" required />
+				</div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input type="tel" name="phone" required />
-          </div>
-        </div>
+				<div>
+					<label>Phone number</label>
+					<div>
+						<input type="tel" name="phone" required />
+					</div>
+				</div>
 
-        <div>
-          <label>Address</label>
-          <div>
-            <input type="text" name="address" required />
-          </div>
-        </div>
+				<div>
+					<label>Address</label>
+					<div>
+						<input type="text" name="address" required />
+					</div>
+				</div>
 
-        <div>
-          <input
-            type="checkbox"
-            name="priority"
-            id="priority"
-            // value={withPriority}
-            // onChange={(e) => setWithPriority(e.target.checked)}
-          />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
-        </div>
+				<div>
+					<input
+						type="checkbox"
+						name="priority"
+						id="priority"
+						// value={withPriority}
+						// onChange={(e) => setWithPriority(e.target.checked)}
+					/>
+					<label htmlFor="priority">Want to yo give your order priority?</label>
+				</div>
 
-        <div>
-          <button>Order now</button>
-        </div>
-      </form>
-    </div>
-  );
+				<div>
+					<button>Order now</button>
+				</div>
+			</form>
+		</div>
+	);
 }
 
 export default CreateOrder;
