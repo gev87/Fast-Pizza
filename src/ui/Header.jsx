0604d210fedac4fcd5833e1455e8960de8596f12
Gevorg Petrosyan <armen.kharatyuan@menugroup.co.uk>
2024-09-30 15:59:01 +0400

    adding AppLayout for the application and showing nested routes in react routers Outlet component

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+function Header() {
+    return (
+			<header>
+				<Link to="/">Fast Pizza Co.</Link>
+				<p>Gevorg</p>
+			</header>
+		);
+}
+
+export default Header
