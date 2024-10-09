44cbb6abc379be4cdc9da05e4902fe6bc9db5d35
Gevorg Petrosyan <armen.kharatyuan@menugroup.co.uk>
2024-10-09 11:25:55 +0400

    submitting forms using React Routers new Form component, React routers action and redirect

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,44 +1,45 @@
 /* eslint-disable no-unused-vars */
 
+import { Form, redirect } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
+
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
-  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
-  );
+	/^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(str);
 
 const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
+	{
+		pizzaId: 12,
+		name: "Mediterranean",
+		quantity: 2,
+		unitPrice: 16,
+		totalPrice: 32,
+	},
+	{
+		pizzaId: 6,
+		name: "Vegetale",
+		quantity: 1,
+		unitPrice: 13,
+		totalPrice: 13,
+	},
+	{
+		pizzaId: 11,
+		name: "Spinach and Mushroom",
+		quantity: 1,
+		unitPrice: 15,
+		totalPrice: 15,
+	},
 ];
 
 function CreateOrder() {
-  // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart;
+	// const [withPriority, setWithPriority] = useState(false);
+	const cart = fakeCart;
 
-  return (
+	return (
 		<div>
 			<h2>Ready to order? Let&aposs go!</h2>
-
-			<form>
+			{/* <Form method="Post" action="/order/new">  the same as the row below */}
+			<Form method="Post">
 				<div>
 					<label>First Name</label>
 					<input type="text" name="customer" required />
@@ -70,11 +71,26 @@ function CreateOrder() {
 				</div>
 
 				<div>
+					<input type="hidden" name="cart" value={JSON.stringify(cart)} />
 					<button>Order now</button>
 				</div>
-			</form>
+			</Form>
 		</div>
 	);
+}
+
+export async function action({ request }) {
+	const formData = await request.formData();
+	const data = Object.fromEntries(formData);
+	
+	const order = {
+		...data,
+		cart: JSON.parse(data.cart),
+		priority: data.priority === "on",
+	};
+	const newOrder = await createOrder(order);
+
+	return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
