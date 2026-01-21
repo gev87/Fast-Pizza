6bc07d13631b1fd9d32f98cc2ea208f58a02f505
Gevorg Petrosyan <armen.kharatyuan@menugroup.co.uk>
2026-01-21 18:08:16 +0400

    adding cutom font from google fonts and styling menu

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,17 +1,22 @@
 import { Link } from 'react-router-dom';
 
-function Button({ children, disabled, to }) {
-  const className =
-    'cursor-pointer rounded-full bg-yellow-400 px-4 py-3 font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed sm:px-6 sm:py-4';
+function Button({ children, disabled, to, type }) {
+  const base =
+    'inline-block cursor-pointer rounded-full bg-yellow-400 font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed ';
+  const styles = {
+    primary: base + ' px-4 py-3 md:px-6 md:py-4',
+    small: base + ' px-4 py-2 text-xs md:px-5 md:py-2.5',
+  };
+
   if (to) {
     return (
-      <Link disabled={disabled} to={to} className={className}>
+      <Link disabled={disabled} to={to} className={styles[type]}>
         {children}
       </Link>
     );
   }
   return (
-    <button disabled={disabled} className={className}>
+    <button disabled={disabled} className={styles[type]}>
       {children}
     </button>
   );
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from '../features/user/Username';
 
 function Header() {
   return (
-    <header className="flex justify-between border-b border-stone-200 bg-yellow-500 sm:px-6 px-4 py-3 uppercase">
+    <header className="flex justify-between border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6 font-pizza ">
       <Link to="/" className="tracking-widest">
         Fast Pizza Co.
       </Link>
